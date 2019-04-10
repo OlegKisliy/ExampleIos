b79b13621784042772ccbb574217e1d80f1a06df
ijk <profugo.canis@gmail.com>
2019-04-10 08:47:19 +0300

    Initial Commit

---
+++ Collection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Collection
+//
+//  Created by ijk on 4/10/19.
+//  Copyright © 2019 x. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
